bcc107b5ddc5dd261bbcd3724db692258b2ad1e9
Sam Fletcher <sfletchertx@gmail.com>
2019-07-01 14:44:09 -0500

    timeline item without revealing draft

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -1,9 +1,12 @@
 var revealButton2020 = document.getElementById("reveal-button-2020");
 var revealButton2019 = document.getElementById("reveal-button-2019");
 var revealButton2018 = document.getElementById("reveal-button-2018");
-var item2020 = document.getElementById("2020");
-var item2019 = document.getElementById("2019");
-var item2018 = document.getElementById("2018");
+var button2020 = document.getElementById("2020");
+var button2019 = document.getElementById("2019");
+var button2018 = document.getElementById("2018");
+var container2020 = document.getElementById("2020-reveal");
+var container2019 = document.getElementById("2019-reveal");
+var container2018 = document.getElementById("2018-reveal");
 
 revealButton2020.onclick = function() {reveal2020()};
 revealButton2019.onclick = function() {reveal2019()};
@@ -11,54 +14,72 @@ revealButton2018.onclick = function() {reveal2018()};
 
 // reveal 2020
 function reveal2020() {
-  if(item2020.classList.contains("timeline-opened"))
+  if(button2020.classList.contains("timeline-opened"))
   {
     // events passed because timeline is OPENED
-    item2020.classList.remove("timeline-opened");
-    item2020.classList.add("timeline-closed");
+    container2020.style.transitionDelay = "0s";
+    container2020.style.transitionDuration = ".2s";
+    container2020.classList.add("hidden");
+    button2020.classList.remove("timeline-opened");
+    button2020.classList.add("timeline-closed");
     // timeline item is now CLOSED
   }
   else
   {
     // events passed because timeline is CLOSED
-    item2020.classList.remove("timeline-closed");
-    item2020.classList.add("timeline-opened");
+    container2020.style.transitionDelay = ".3s";
+    container2020.style.transitionDuration = "var(--transition-time)";
+    container2020.classList.remove("hidden");
+    button2020.classList.remove("timeline-closed");
+    button2020.classList.add("timeline-opened");
     // timeline item is now OPENED
   }
 }
 
 // reveal 2019
 function reveal2019() {
-  if(item2019.classList.contains("timeline-opened"))
+  if(button2019.classList.contains("timeline-opened"))
   {
     // events passed because timeline is OPENED
-    item2019.classList.remove("timeline-opened");
-    item2019.classList.add("timeline-closed");
+    container2019.style.transitionDelay = "0s";
+    container2019.style.transitionDuration = ".2s";
+    container2019.classList.add("hidden");
+    button2019.classList.remove("timeline-opened");
+    button2019.classList.add("timeline-closed");
     // timeline item is now CLOSED
   }
   else
   {
     // events passed because timeline is CLOSED
-    item2019.classList.remove("timeline-closed");
-    item2019.classList.add("timeline-opened");
+    container2019.style.transitionDelay = ".3s";
+    container2019.style.transitionDuration = "var(--transition-time)";
+    container2019.classList.remove("hidden");
+    button2019.classList.remove("timeline-closed");
+    button2019.classList.add("timeline-opened");
     // timeline item is now OPENED
   }
 }
 
 // reveal 2018
 function reveal2018() {
-  if(item2018.classList.contains("timeline-opened"))
+  if(button2018.classList.contains("timeline-opened"))
   {
     // events passed because timeline is OPENED
-    item2018.classList.remove("timeline-opened");
-    item2018.classList.add("timeline-closed");
+    container2018.style.transitionDelay = "0s";
+    container2018.style.transitionDuration = ".2s";
+    container2018.classList.add("hidden");
+    button2018.classList.remove("timeline-opened");
+    button2018.classList.add("timeline-closed");
     // timeline item is now CLOSED
   }
   else
   {
     // events passed because timeline is CLOSED
-    item2018.classList.remove("timeline-closed");
-    item2018.classList.add("timeline-opened");
+    container2018.style.transitionDelay = ".3s";
+    container2018.style.transitionDuration = "var(--transition-time)";
+    container2018.classList.remove("hidden");
+    button2018.classList.remove("timeline-closed");
+    button2018.classList.add("timeline-opened");
     // timeline item is now OPENED
   }
 }
