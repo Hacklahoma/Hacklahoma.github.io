fc3d9300942a26cbf482799da6e6181c00a447fb
Sam Fletcher <sfletchertx@gmail.com>
2019-09-12 19:50:25 -0500

    code cleanup/removed old hamburger code

--- scripts/2020/scripts.js
+++ scripts/2020/scripts.js
@@ -1,4 +1,4 @@
-// reveals background of nav bar on scroll
+/* reveals background of nav bar on scroll */
 update();
 window.onscroll = function() {
   update();
@@ -15,8 +15,7 @@ function update() {
   }
 }
 
-
-// manages hamburger menu
+/* manages hamburger menu */
 const hamburger = document.querySelector(".hamburger");
 const lines = document.querySelectorAll(".hamburger .line");
 const navLinks = document.querySelector("nav");
@@ -28,6 +27,7 @@ hamburger.addEventListener("click", () => {
   });
 });
 
+// makes sure the hamburger menu is closed when entering mobile view
 addEventListener("resize", () => {
   if(window.innerWidth > 720 && navLinks.classList.contains("open")) {
     navLinks.classList.toggle("open");
@@ -36,33 +36,3 @@ addEventListener("resize", () => {
     });
   }
 });
-
-
-// var open = false;
-// var menu = document.getElementsByClassName("hamburger-menu-item");
-// var navbar = document.getElementById("navbar");
-// addEventListener("resize", handleResize);
-//
-// function handleResize() {
-//   if(window.innerWidth > 720)
-//     if(open = true)
-//       hamburger()
-// }
-//
-// function hamburger() {
-//   if(open) { // if currently opened
-//     menu[0].classList.remove("hamburger-open-top");
-//     menu[1].classList.remove("hamburger-open-center");
-//     menu[2].classList.remove("hamburger-open-bottom");
-//     navbar.classList.add("hamburger-navbar-close");
-//     open = false;
-//   }
-//   else { // if currently closed
-//     menu[0].classList.add("hamburger-open-top");
-//     menu[1].classList.add("hamburger-open-center");
-//     menu[2].classList.add("hamburger-open-bottom");
-//     navbar.classList.remove("hamburger-navbar-close");
-//     open = true;
-//   }
-//
-// }
