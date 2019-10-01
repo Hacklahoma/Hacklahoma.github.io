8615719cffb7b75d3a98c0f741fed74fddaf6b08
s-fletcher <sfletchertx@gmail.com>
2019-10-01 14:25:55 -0500

    added moving clouds in the back

---
+++ scripts/2020/clouds.js
@@ -0,0 +1,34 @@
+var cloudRefs = document.getElementsByClassName("cloud");
+
+var clouds = [];
+
+for(var i = 0; i < cloudRefs.length; i++) {
+  var cloud = {
+    v: Math.random() * (.6 - .2) + .2, // velocity at which cloud moves
+    pos: Math.random() * (window.innerWidth - (-140)) + (-140), // position of cloud from the left
+    ref: cloudRefs[i] // the reference
+  };
+  clouds.push(cloud);
+}
+
+function step(timestamp) { // each frame
+  for(var i = 0; i < clouds.length; i++) {
+    if(clouds[i].pos > window.innerWidth) {
+      clouds[i].pos = -140;
+    }
+    clouds[i].pos += clouds[i].v;
+    clouds[i].ref.style.left = "" + clouds[i].pos + "px";
+  }
+  window.requestAnimationFrame(step); // tick tock
+}
+
+window.requestAnimationFrame(step);
+
+handleResize();
+function handleResize() {
+}
+
+
+function Point(x,y) {
+
+}
