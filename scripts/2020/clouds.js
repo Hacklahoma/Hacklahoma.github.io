88141b96ff00ee40c35982f9ac2883ee296a758e
s-fletcher <sfletchertx@gmail.com>
2019-10-02 11:59:58 -0500

    randomized clouds on resize

--- scripts/2020/clouds.js
+++ scripts/2020/clouds.js
@@ -1,11 +1,11 @@
 var cloudRefs = document.getElementsByClassName("cloud");
-var resize = false;
 var clouds = [];
+var parentWidth = cloudRefs[0].parentElement.clientWidth;
 
 for(var i = 0; i < cloudRefs.length; i++) {
   var cloud = {
     v: Math.random() * (.25 - .1) + .1, // velocity at which cloud moves
-    pos: Math.random() * (cloudRefs[i].parentElement.clientWidth - (-140)) + (-140), // position of cloud from the left
+    pos: Math.random() * (parentWidth - (-140)) + (-140), // position of cloud from the left
     ref: cloudRefs[i] // the reference
   };
   clouds.push(cloud);
@@ -13,7 +13,7 @@ for(var i = 0; i < cloudRefs.length; i++) {
 
 function step(timestamp) { // each frame
   for(var i = 0; i < clouds.length; i++) {
-    if(clouds[i].pos > clouds[i].ref.parentElement.clientWidth) {
+    if(clouds[i].pos > parentWidth) {
       clouds[i].pos = -140;
     }
     clouds[i].pos += clouds[i].v;
@@ -21,4 +21,18 @@ function step(timestamp) { // each frame
   }
   window.requestAnimationFrame(step); // tick tock
 }
+
 window.requestAnimationFrame(step);
+
+window.handleResize
+
+window.addEventListener("resize", handleResize);
+
+function handleResize() {
+  if(parentWidth != clouds[0].ref.parentElement.clientWidth) {
+    parentWidth = clouds[0].ref.parentElement.clientWidth;
+    for(var i = 0; i < clouds.length; i++) {
+      clouds[i].pos = Math.random() * (parentWidth - (-140)) + (-140);
+    }
+  }
+}
